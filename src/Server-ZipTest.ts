5fa260394137ee9473bdbca9a33a39d5da085251
deadlydog <deadlydog@hotmail.com>
2020-08-15 00:01:58 -0600

    Get ZipTest.tx working

--- src/Server-ZipTest.ts
+++ src/Server-ZipTest.ts
@@ -1,17 +1,33 @@
 import * as AdmZip from 'adm-zip';
-// var AdmZip = require('adm-zip');
+import * as fs from 'fs';
 
-var sourcePath = "C:/Temp/Test.zip";
+var sourcePath = "C:\\Temp\\Temp.zip";
+
+if (!fs.existsSync(sourcePath))
+{
+	// console.error(`The file '${sourcePath}' does not exist.`)
+	throw `The specified file '${sourcePath}' does not exist.`
+}
 
 var sourcePathIsZipFile = false;
-var sourcePathZipDirectoryPath = sourcePath + "-Extracted/";
-if (sourcePath.endsWith(".zip")) {
+var sourcePathZipDirectoryPath = sourcePath + "-Extracted";
+if (sourcePath.endsWith(".zip"))
+{
 	sourcePathIsZipFile = true;
 
-	console.log("is a zip file");
+	console.log(`'${sourcePath}' is a zip file, and it will be extracted to '${sourcePathZipDirectoryPath}'.`);
 	var zip = new AdmZip(sourcePath);
 	zip.extractAllTo(sourcePathZipDirectoryPath)
 }
-else {
+else
+{
 	console.log("is NOT a zip file");
+}
+
+if (sourcePathIsZipFile)
+{
+	console.log(`Zipping up directory '${sourcePathZipDirectoryPath}' to file '${sourcePath}'.`)
+	var zip = new AdmZip();
+	zip.addLocalFolder(sourcePathZipDirectoryPath);
+	zip.writeZip(sourcePath);
 }
