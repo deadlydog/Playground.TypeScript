11d47df8c2694474b9179c9c9fb2033e6b09e0af
deadlydog <deadlydog@hotmail.com>
2020-08-15 00:07:13 -0600

    Use const and let instead of var

--- src/Server-ZipTest.ts
+++ src/Server-ZipTest.ts
@@ -1,7 +1,7 @@
 import * as AdmZip from 'adm-zip';
 import * as fs from 'fs';
 
-var sourcePath = "C:\\Temp\\Temp.zip";
+const sourcePath: string = "C:\\Temp\\Temp.zip";
 
 if (!fs.existsSync(sourcePath))
 {
@@ -9,14 +9,14 @@ if (!fs.existsSync(sourcePath))
 	throw `The specified file '${sourcePath}' does not exist.`
 }
 
-var sourcePathIsZipFile = false;
-var sourcePathZipDirectoryPath = sourcePath + "-Extracted";
+let sourcePathIsZipFile: boolean = false;
+const sourcePathZipDirectoryPath: string = sourcePath + "-Extracted";
 if (sourcePath.endsWith(".zip"))
 {
 	sourcePathIsZipFile = true;
 
 	console.log(`'${sourcePath}' is a zip file, and it will be extracted to '${sourcePathZipDirectoryPath}'.`);
-	var zip = new AdmZip(sourcePath);
+	const zip = new AdmZip(sourcePath);
 	zip.extractAllTo(sourcePathZipDirectoryPath)
 }
 else
@@ -27,7 +27,7 @@ else
 if (sourcePathIsZipFile)
 {
 	console.log(`Zipping up directory '${sourcePathZipDirectoryPath}' to file '${sourcePath}'.`)
-	var zip = new AdmZip();
+	const zip = new AdmZip();
 	zip.addLocalFolder(sourcePathZipDirectoryPath);
 	zip.writeZip(sourcePath);
 }
